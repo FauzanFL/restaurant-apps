6dc59204bc1725e3ae7d24b7e5e25754a244f4f3
Fauzan FL <fauzanarth@gmail.com>
2023-02-12 12:24:36 +0700

    fixing lazyload: restaurant-item.js

--- src/scripts/components/restaurant-item.js
+++ src/scripts/components/restaurant-item.js
@@ -14,19 +14,19 @@ class RestaurantItem extends HTMLElement {
     this.innerHTML = `
     <p tabindex="0" class="restaurant__rate">⭐️ ${this._restaurant.rating}</p>
     <h3 tabindex="0" class="restaurant__title skeleton"><a class="resto__link" href="/#/detail/${
-  this._restaurant.id
-}">${this._restaurant.name}</a></h3>
+      this._restaurant.id
+    }">${this._restaurant.name}</a></h3>
     <div class="skeleton">
-      <img class="lazyload" tabindex="0" src="${`${CONFIG.BASE_IMG_URL}small/${this._restaurant.pictureId}`}" alt="gambar dari ${
-  this._restaurant.name
-}">
+      <img class="lazyload" tabindex="0" data-src="${`${CONFIG.BASE_IMG_URL}small/${this._restaurant.pictureId}`}" alt="gambar dari ${
+      this._restaurant.name
+    }">
     </div>
     <p tabindex="0" class="restaurant__city skeleton">Di kota: ${
-  this._restaurant.city
-}</p>
+      this._restaurant.city
+    }</p>
     <p tabindex="0" class="restaurant__description skeleton">${
-  this._restaurant.description
-}</p>
+      this._restaurant.description
+    }</p>
     `;
     this.classList.add('restaurant__item');
   }
